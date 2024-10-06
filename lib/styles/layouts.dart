7a96c4d60ba0dcb68eb9fab05a214933acfde352
Magda <266857@student.pwr.edu.pl>
2024-10-06 22:40:37 +0200

    add login screen

--- lib/styles/layouts.dart
+++ lib/styles/layouts.dart
@@ -50,3 +50,78 @@ class Button extends StatelessWidget {
     );
   }
 }
+
+class ElevatedCustomButton extends StatelessWidget {
+  final Widget text;
+  final VoidCallback? onPressed; // Allow null for onPressed
+
+  const ElevatedCustomButton({required this.text, required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          // padding: const EdgeInsets.symmetric(vertical: 16.0),
+          minimumSize: const Size(150, 50),
+          side: const BorderSide(color: Colors.white, width: 1.5),
+          backgroundColor: Colors.white.withOpacity(0.2),
+          foregroundColor: Colors.white,
+          textStyle: Theme.of(context).textTheme.titleLarge,
+          elevation: 0,
+        ),
+        child: text,
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool obscureText;
+  final Widget? suffixIcon; // Allow suffix icon
+
+  const CustomTextField({
+    required this.controller,
+    required this.labelText,
+    this.obscureText = false,
+    this.suffixIcon,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.white), // Text color
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(color: Colors.white), // Label color
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: const BorderSide(color: Colors.white70), // Gray border
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: const BorderSide(color: Colors.white70), // Gray border on focus
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: const BorderSide(color: Colors.white70), // Gray border when enabled
+        ),
+        suffixIcon: suffixIcon, // Use the optional suffix icon
+      ),
+    );
+  }
+}
+
+
+
