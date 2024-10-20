5f56d60b44acbb2a381fc1d59db6c496e9c993f8
malgosielska <malgosielska@hmail.com>
2024-10-20 11:48:23 +0200

    Changing languages works dynamically now

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:feedback/feedback.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'app.dart';
-import 'languages/feedback_language.dart';
-import 'languages/localization_utils.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await LocalizationUtils.instance.initialize();
-
-  runApp(
-      BetterFeedback(
-        localeOverride: LocalizationUtils.instance.locale,
-        localizationsDelegates: [
-          CustomFeedbackLocalizationsDelegate(),
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        child: const App(),
-      )
-  );
-}
+void main() => runApp(const App());
