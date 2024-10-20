62627f06ed4190bfb45b3c72bea5e18a2fa1806c
malgosielska <malgosielska@hmail.com>
2024-10-20 11:31:18 +0200

    Added language support for feedback

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:feedback/feedback.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'app.dart';
+import 'languages/feedback_language.dart';
 import 'languages/localization_utils.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,5 +13,16 @@ void main() async {
   await LocalizationUtils.instance.initialize();
 
   runApp(
-      const BetterFeedback(child:App()));
+      BetterFeedback(
+        localeOverride: LocalizationUtils.instance.locale,
+        localizationsDelegates: [
+          CustomFeedbackLocalizationsDelegate(),
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        child: const App(),
+      )
+  );
 }
