855a8169e8a68dd59d5741f0bc585f10856b2b38
izabela <iza.kalenik@gmail.com>
2024-10-21 21:01:48 +0200

    Tested firebase connection - it works!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+// import 'package:cloud_firestore/cloud_firestore.dart'; //Uncomment for tests
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'app.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  //addData(); //For testing the connection with database (instances are added to the Firestore Database)
   runApp(const App());
 }
+
+// void addData() {
+//   FirebaseFirestore.instance.collection('test').add({'name': 'Test Item'});
+// }
+
