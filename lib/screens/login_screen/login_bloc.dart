50190e1ec50833d9f5a733d8c3f27a836ad6e6a1
Magda Szeląg <95686924+cichuss@users.noreply.github.com>
2024-10-22 20:24:06 +0100

    add login functionality and fix login overflow (#31)
    * add login functionality and fix login overflow
    
    * add languages
    
    ---------
    
    Co-authored-by: Magda <266857@student.pwr.edu.pl>

--- lib/screens/login_screen/login_bloc.dart
+++ lib/screens/login_screen/login_bloc.dart
@@ -1,24 +1,43 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   LoginBloc() : super(LoginInitial()) {
     on<LoginButtonPressed>(_onLoginButtonPressed);
   }
 
   Future<void> _onLoginButtonPressed(
-    LoginButtonPressed event,
-    Emitter<LoginState> emit,
-  ) async {
-    emit(LoginLoading());
 
+      LoginButtonPressed event,
+      Emitter<LoginState> emit,
+      ) async {
+    emit(LoginLoading());
+    final localizations = event.localizations;
     try {
-      // Replace this with your own login logic
-      await Future.delayed(const Duration(seconds: 2));
+      // UserCredential userCredential =
+      await _auth.signInWithEmailAndPassword(
+        email: event.email,
+        password: event.password,
+      );
       emit(LoginSuccess());
+    } on FirebaseAuthException catch (e) {
+
+      String errorMessage;
+      if (e.code == 'invalid-email') {
+        errorMessage = localizations.not_email;
+      } else if (e.code == 'invalid-credential') {
+        errorMessage = localizations.invalid_credentials;
+      } else {
+        errorMessage = localizations.unknown_error;
+      }
+      emit(LoginFailure(error: errorMessage));
     } catch (error) {
       emit(LoginFailure(error: error.toString()));
     }
