bef5ae35fab823ad637dde94b2ecc5fe6dfdd222
Magda <266857@student.pwr.edu.pl>
2024-11-01 20:55:51 +0100

    add fetching liked movies dynamically

--- lib/screens/liked_movies_screen.dart
+++ lib/screens/liked_movies_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 //import '../models/movie_model.dart';
 import 'package:zpi_project/movies/domain/entities/movie.dart';
+import '../database_configuration/firestore_service.dart';
+import '../movies/data/data_sources/movie_remote_data_source.dart';
+import '../movies/data/repositories/movie_repository_impl.dart';
 import '../styles/layouts.dart';
 import '../utils/check_login_status.dart';
 import '../widgets/category_selector.dart';
@@ -17,77 +20,28 @@ class LikedMoviesScreen extends StatefulWidget {
 }
 
 class _LikedMoviesScreenState extends State<LikedMoviesScreen> {
+  List<Movie> _likedMovies = [];
   List<String> _selectedCategories = [];
 
   @override
   void initState() {
     super.initState();
     checkLoginStatus(context);
+    _fetchLikedMovies();
   }
 
-  final List<Movie> _allMovies = [
-    Movie(
-      id: 1,
-      title: 'Avengers',
-      overview: 'Avengers saving the world...',
-      posterPath:
-          'https://image.tmdb.org/t/p/original/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg',
-      voteAverage: 8.5,
-      releaseDate: '2012-05-04',
-      isForAdults: false,
-      categories: ['Action', 'Sci-Fi'],
-    ),
-    Movie(
-      id: 2,
-      title: 'Forrest Gump',
-      overview: 'Life story of Forrest...',
-      posterPath:
-          'https://image.tmdb.org/t/p/original/iixrNXX79OR7knBx1i9S51PfVlz.jpg',
-      voteAverage: 8.8,
-      releaseDate: '1994-07-06',
-      isForAdults: false,
-      categories: ['Drama', 'Romance'],
-    ),
-    Movie(
-      id: 533535,
-      title: 'Deadpool & Wolverine',
-      overview: 'Wolverine teams up with Deadpool...',
-      posterPath:
-          'https://image.tmdb.org/t/p/w500/7Hi6mRLsQtTaEtKiHqSeRFR1TQ2.jpg',
-      voteAverage: 7.722,
-      releaseDate: "2024-07-26",
-      isForAdults: false,
-      categories: ['Action', 'Comedy', 'Documentary'],
-    ),
-    Movie(
-      id: 3,
-      title: 'The Notebook',
-      overview:
-          'A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom. They enjoy a romantic summer together but are soon separated.',
-      posterPath:
-          'https://image.tmdb.org/t/p/original/rNzQyW4f8B8cQeg7Dgj3n6eT5k9.jpg',
-      voteAverage: 7.8,
-      releaseDate: '2004-06-25',
-      isForAdults: false,
-      categories: ['Drama', 'Romance'],
-    ),
-    Movie(
-      id: 4,
-      title: 'Mean Girls',
-      overview:
-          'Cady Heron is a hit with The Plastics, the A-list girl clique at her new school. But her popularity comes with a price.',
-      posterPath:
-          'https://image.tmdb.org/t/p/original/fXm3YKXAEjx7d2tIWDg9TfRZtsU.jpg',
-      voteAverage: 7.0,
-      releaseDate: '2004-04-30',
-      isForAdults: false,
-      categories: ['Comedy', 'Teen'],
-    ),
-  ];
+  Future<void> _fetchLikedMovies() async {
+    final userLikedMovieIds = await FirestoreService().getLikedMovies();
+    final likedMovies = await MovieRepositoryImpl(MovieRemoteDataSource())
+        .fetchLikedMovies(userLikedMovieIds);
+    setState(() {
+      _likedMovies = likedMovies;
+    });
+  }
 
   List<Movie> get _filteredMovies {
-    if (_selectedCategories.isEmpty) return _allMovies;
-    return _allMovies.where((movie) {
+    if (_selectedCategories.isEmpty) return _likedMovies;
+    return _likedMovies.where((movie) {
       return movie.categories
           .any((category) => _selectedCategories.contains(category));
     }).toList();
